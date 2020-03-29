93c50c033f165591c3075940ca6510bc991a09b3
Lionel Desorme <lionel.desorme@free.fr>
2020-03-29 19:12:52 +0200

    Test cases

--- TestLines/Program.cs
+++ TestLines/Program.cs
@@ -47,6 +47,44 @@ namespace TestLines
 
             }
 
+            // Selectionner 4 lignes parmi les 20 avec ordre
+            // 20 * 19 * 18 * 17 = 116 280
+            // generer les 20^4 cas et eliminer les doublons
+
+            var testCase = new List<Tuple<int, int, int, int>>();
+            for (int i = 0; i < lines.Count; i++)
+                for (int j = 0; j < lines.Count; j++)
+                    for (int k = 0; k < lines.Count; k++)
+                        for (int l = 0; l < lines.Count; l++)
+                        {
+                            if (i != j && i != k && i != l && j != k && j != l && k != l)
+                                testCase.Add(new Tuple<int, int, int, int>(i, j, k, l));
+                        }
+            Console.WriteLine(testCase.Count);
+
+            int ok = 0;
+            foreach (var test in testCase)
+            {
+                // Tester que 2 ligne consecutives ne sont pas paralleles (le crayon ne se leve pas)
+                int i = test.Item1;
+                int j = test.Item2;
+                int k = test.Item3;
+                int l = test.Item4;
+
+                if (!lines[i].IsParralel(lines[j]) &&
+                    !lines[j].IsParralel(lines[k]) &&
+                    !lines[k].IsParralel(lines[l]))
+                {
+                    ok++;
+                    // checker les points
+
+
+
+
+                }
+
+            }
+            Console.WriteLine(ok);
         }
     }
 }
